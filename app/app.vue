1d4f8000fb7b1843ce452dcc5153c7622e6b163e
Walidbenmoussa <163693737+Walidbenmoussa@users.noreply.github.com>
2025-10-15 22:21:38 +0200

    Update IP cookie value to production URL

--- app/app.vue
+++ app/app.vue
@@ -12,7 +12,7 @@
 <script setup lang="ts">
 import { fr } from '@nuxt/ui/locale'
 const IP = useCookie('IP')
-IP.value = 'http://127.0.0.1:8000'
+IP.value = 'https://gestion-projet-vgog.onrender.com'
 useHead({
   meta: [
     { charset: 'utf-8' },
