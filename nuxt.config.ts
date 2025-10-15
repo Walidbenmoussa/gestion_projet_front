1fdb9b14cd5d742d004cf810818673d592225c4e
Walidbenmoussa <163693737+Walidbenmoussa@users.noreply.github.com>
2025-10-15 21:32:04 +0200

    Fix syntax by removing extra comma
    Removed unnecessary comma in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr:false,
-
-  ,
   modules: ['@nuxt/eslint', '@nuxt/ui', 'nuxt-icon'],
 
   devtools: {
