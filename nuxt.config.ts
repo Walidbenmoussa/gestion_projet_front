5d191271506638cb6075b129ca14b78a68978fe3
Walidbenmoussa <163693737+Walidbenmoussa@users.noreply.github.com>
2025-10-16 00:33:49 +0200

    Enable devtools and clean up nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr:false,
-  modules: ['@nuxt/eslint', '@nuxt/ui', 'nuxt-icon'],
+  // Désactive le rendu côté serveur
+  ssr: false,
 
+  // Modules Nuxt à inclure
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    'nuxt-icon'
+  ],
+
+  // Outils de développement activés
   devtools: {
-    enabled: false
+    enabled: true
   },
 
+  // Fichier CSS global
   css: ['~/assets/css/main.css'],
 
+  // Règles de routage
   routeRules: {
-    '/': { prerender: true },
-    
+    '/': { prerender: true }
   },
 
+  // Date de compatibilité pour activer certaines fonctionnalités de Nuxt
   compatibilityDate: '2025-01-15',
 
+  // Configuration d’ESLint
   eslint: {
     config: {
       stylistic: {
         commaDangle: 'never',
         braceStyle: '1tbs'
       }
+    }
+  },
+
+  // Configuration de l'en-tête HTML (balises meta, etc.)
+  app: {
+    head: {
+      meta: [
+        { name: 'format-detection', content: 'telephone=no' }
+      ]
     }
   }
 })
