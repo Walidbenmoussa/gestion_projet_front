25fc66c96ef215e910c2f762b2e9ba9294fd3b23
Walidbenmoussa <163693737+Walidbenmoussa@users.noreply.github.com>
2025-10-16 20:46:02 +0200

    Update test.vue

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,7 +1,55 @@
 <template>
-  <div class="pt-12 w">
-<Service/>
-</div>
+<template>
+  <UHeader title="Page test" :toggle="false">
+     <template #right>
+    </template>
+  </UHeader>
+
+  <UPageGrid class="p-6">
+    
+    <UCard class="w-sm">
+      <template #header>
+      <h1>Bonjour  </h1>
+      </template>
+      <div>
+        Bonjour body
+       </div> 
+    </UCard>
+ 
+    <UCard class="w-sm">
+      <template #header>
+      <h1>Bonjour  </h1>
+      </template>
+      <div>
+        Bonjour body
+       </div> 
+    </UCard>
+      <UCard class="w-sm">
+      <template #header>
+      <h1>Bonjour  </h1>
+      </template>
+      <div>
+        Bonjour body
+       </div> 
+    </UCard>
+      <UCard class="w-sm">
+      <template #header>
+      <h1>Bonjour  </h1>
+      </template>
+      <div>
+        Bonjour body
+       </div> 
+    </UCard>
+      <UCard class="w-sm">
+      <template #header>
+      <h1>Bonjour  </h1>
+      </template>
+      <div>
+        Bonjour body
+       </div> 
+    </UCard>
+    
+  </UPageGrid>
 
 </template>
 
